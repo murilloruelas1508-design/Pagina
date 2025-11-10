7e0b5a22fb07fe49661194ab130b73b4536608de
cri0108r <cr.rodriguez0101@gmail.com>
2025-11-10 11:53:45 -0700

    Primer commit

--- Pagina1.php
+++ Pagina1.php
@@ -1,6 +1,6 @@
 
 <?php
-echo "<h1 style='color: blue;'>Submódulo 2 (actualizado)</h1>";
+echo "<h1 style='color: blue;'>Submódulo 2 gi</h1>";
 ?>
-<h1 style="color: green;">Submódulo 1 (versión nueva)</h1>
+<h1 style="color: green;">Submódulo 1 </h1>
 <p>Este texto fue agregado para probar la sincronización con GitHub.</p>
