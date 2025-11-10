f50da8c90819dbda5a9b7158ba65dcdd8b8d618d
Sakura Murillo <sakuramurilloruelas@gmail.com>
2025-11-09 20:01:41 -0700

    Prueba de sincronización con cambio visible

--- Pagina1.php
+++ Pagina1.php
@@ -1,5 +1,6 @@
 
 <?php
-echo "<h1>Submodulo 2</h1>";
+echo "<h1 style='color: blue;'>Submódulo 2 (actualizado)</h1>";
 ?>
-<h1>Submodulo 1</h1>
+<h1 style="color: green;">Submódulo 1 (versión nueva)</h1>
+<p>Este texto fue agregado para probar la sincronización con GitHub.</p>
